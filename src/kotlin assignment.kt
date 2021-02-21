53545f3c9bf460a07c8b800e9166e8dd57985d59
beldinneema <beldineneema@gmail.com>
2021-02-21 11:50:28 +0300

    assignment1

---
+++ src/kotlin assignment.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val fullName = "beldine neema"
+    var age = 21
+    var phoneNumber = "0704948928"
+    var weight = 53
+    val citizen = "kenyan"
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizen)
+
+}
